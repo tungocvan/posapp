4521c53b73139d5be3908b82bfbcdcd8c6184030
tungocvan <tungocvan@gmail.com>
2019-09-04 14:32:42 +0700

    possapp lan3

---
+++ precache-manifest.ec3b7d535f368b7dc94d72540d742d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8fd2604cb0f717d3d5fe69ac3c6b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d44c7f9273dd13631e",
+    "url": "/static/css/main.fe0ea3d8.chunk.css"
+  },
+  {
+    "revision": "75ef3e946939562c2e6a",
+    "url": "/static/js/2.736bdcdf.chunk.js"
+  },
+  {
+    "revision": "f4d44c7f9273dd13631e",
+    "url": "/static/js/main.b0b0763a.chunk.js"
+  },
+  {
+    "revision": "835cae36aaa8e6a9c8db",
+    "url": "/static/js/runtime~main.5b93e505.js"
+  }
+]);
